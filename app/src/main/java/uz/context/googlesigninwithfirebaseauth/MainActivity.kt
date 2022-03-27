cc36a7bf4228fb1998fd42b6a3beadaec38ffd7c
Samandar <samandarcoderdev@gmail.com>
2022-03-27 21:28:40 +0500

    Sign in With Google account in Firebase

--- app/src/main/java/uz/context/googlesigninwithfirebaseauth/MainActivity.kt
+++ app/src/main/java/uz/context/googlesigninwithfirebaseauth/MainActivity.kt
@@ -46,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE) {
